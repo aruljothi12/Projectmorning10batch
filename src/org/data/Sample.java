054d20846aad3d5691a13950b847173e8a1bbe7d
aruljothi12 <arulrajakings12@gmail.com>
2023-03-25 20:59:43 +0530

    ram added this code

--- src/org/data/Sample.java
+++ src/org/data/Sample.java
@@ -2,8 +2,8 @@ package org.data;
 
 public class Sample {
 	
-	public void data1() {
-		System.out.println("thi is my page");
+	public void addnew() {
+		System.out.println("thi is my page ram");
 	}
 	public static void main(String[] args) {
 		System.out.println("the pass");
