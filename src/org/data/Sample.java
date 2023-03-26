4c59e92767a0eddd136bc892ee3ddf57aba842ee
aruljothi12 <arulrajakings12@gmail.com>
2023-03-26 22:03:36 +0530

    sam added

--- src/org/data/Sample.java
+++ src/org/data/Sample.java
@@ -1,5 +1,16 @@
 package org.data;
 
+import java.nio.charset.MalformedInputException;
+
 public class Sample {
+	
+	private void m2() {
+		System.out.println("this is sam");
+	
+
+	}
+	public static void main(String[] args) {
+		System.out.println("sam........");
+	}
 
 }
